7cb131e01cd014da68e8731dc116672cf121fdbe
sobadrush <sobadrush@icloud.com>
2022-12-02 00:42:45 +0800

    1st commit

---
+++ CodeFirst_Demo/Migrations/20221201163246_Initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodeFirstDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        /// Up 的部分是 - 升級到這個版本所需要的程式碼
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Age", "Email", "Name", "UserName" },
+                values: new object[,]
+                {
+                    { 1, 23, "test111@gmail.com.tw", "Roger Lo", "友寄隆輝" },
+                    { 2, 24, "test222@gmail.com.tw", "Doraemon", "多啦A夢" },
+                    { 3, 25, "test333@gmail.com.tw", "Pikachu", "皮卡丘" }
+                });
+        }
+
+        /// <inheritdoc />
+        /// Down 的部分是 - 從這個版本降級到前一個版本所需要的程式碼
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Users");
+        }
+    }
+}
---
+++ CodeFirst_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CodeFirst_Demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
