a650d3a5fb2fb3fc61f1254949621103dea4a558
sobadrush <sobadrush@icloud.com>
2022-12-08 01:00:40 +0800

    新增自訂錯誤頁

--- CodeFirst_Demo/Program.cs
+++ CodeFirst_Demo/Program.cs
@@ -20,10 +20,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error"); // 未捕捉的Exception程式錯誤，要執行的Controller與Action(必須 / 開頭，否則會發生An error occurred while starting the application)
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage(); // 當 env.EnvironmentName 為 Development，程式出現詳細的錯誤訊息
+    // app.UseExceptionHandler("/Home/Error");
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -39,5 +45,8 @@ app.MapHealthChecks("/healthz");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//當用戶輸入的網址找不到時↓
+app.UseStatusCodePagesWithRedirects("~/404.html"); //或直接給http開頭的絕對URL
 
 app.Run();
